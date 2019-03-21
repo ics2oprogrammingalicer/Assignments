9dfbc7f5231eae5e98b184f2606f1bdbe47dcd8a
Alice Roberts <alice.roberts@ocsbstudent.ca>
2019-03-21 08:18:37 -0400

    added stuff

--- anammatingimagesAlicer/main.lua
+++ anammatingimagesAlicer/main.lua
@@ -14,15 +14,15 @@ local backgroundImage = display.newImageRect("Images/sunsetbackground.jpg", 2048
 
 -- charecter images with width and height
 local bird = display.newImageRect("Images/bird.png", 200, 200)
-local hotairballoon = display.newImageRect("Images/hotairballoon.png", 100, 100)
-local airplane = display.newImageRect("Images/plane.png")
+local Hotairballoon = display.newImageRect("Images/hotairballoon.png", 100, 100)
+local airplane = display.newImageRect("Images/plane.png", 250, 300)
 
 -- set image to be transparent
 bird.alpha = 1
 
 -- set initial x and y position of bird
 bird.x = 0
-bird.y = display.contentHeight/
+bird.y = display.contentHeight/2
 
 -- Function: moveBird
 -- imput: this function accepts an event listener
@@ -38,5 +38,55 @@ end
 -- MoveShip will be called over and over again 
 Runtime:addEventListener("enterFrame", MoveBird)
 
+-- global variables 
+scrollspeed = 3
 
+---------------------------------------------------------------------------
+ -- set image to be transparent
+Hotairballoon = 1
 
+ -- set initial x and y position
+ Hotairballoon = 0
+ Hotairballoon = display.contentHeight/1
+
+ -- function: moveHotairballon
+ -- input: this function accepts an event listener
+ -- output: none
+ -- discription: this function adds the scroll speed to the x value of the ship
+ local function MoveHotairballon(event)
+ 	-- add the scrolol speed to the x value of the ship
+ 	Hotairballoon = Hotairballoon + scrollSpeed
+ 	-- change the transparencey of the ship every time it moves so that it fades out
+ 	Hotairballoon = Hotairballoon + 0.01
+end
+
+-- moveship will be called over and over again
+Runtime:addEventListener("enterFrame", MoveHotairballon)
+
+-- global variables
+scrollspeed = 5
+
+---------------------------------------------------------------------------
+ -- set image to be transparent
+airplane = 1
+
+ -- set initial x and y position
+ airplane = 0
+ airplane = display.contentHeight/1
+
+ -- function: moveHotairballon
+ -- input: this function accepts an event listener
+ -- output: none
+ -- discription: this function adds the scroll speed to the x value of the ship
+ local function MoveAirplane(event)
+ 	-- add the scrolol speed to the x value of the ship
+ 	airplane = airplane + scrollSpeed
+ 	-- change the transparencey of the ship every time it moves so that it fades out
+ 	airplane = airplane + 0.01
+end
+
+-- moveship will be called over and over again
+Runtime:addEventListener("enterFrame", MoveAirplane)
+
+-- global variables
+scrollspeed = 5
