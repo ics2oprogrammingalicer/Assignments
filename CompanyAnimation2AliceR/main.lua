7a09ec919abf16db6a88e21b156f72fb604db16d
Alice Roberts <47784287+s436512@users.noreply.github.com>
2019-04-10 09:44:05 -0400

    added stuff

---
+++ CompanyAnimation2AliceR/main.lua
@@ -0,0 +1,37 @@
+-- Title: CompanyAnimation
+-- Name: Alice R
+-- Course: ICS2O/3C
+-- This program
+---------------------------------------------------------------------------------
+-- hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- set background colour
+display.setDefault("background", 0/255, 68/255, 204/255)
+
+---------------------------------------------------------------------------------
+-- GLOBAL AND LOCAL VARIABLES
+---------------------------------------------------------------------------------
+-- set the scroll speed
+scrollSpeed = 3
+
+-- company logo with width and height
+local leftSide = display.newImageRect("Images/logoLeftSide.png", 300, 400)
+local rightSide = display.newImageRect("Images/logoRightSide.png", 300, 400)
+local logo = display.newImageRect("Images/CompanyLogoAliceR.png", 300, 400)
+
+-- make logo invisible
+logo.isVisible = false
+---------------------------------------------------------------------------------
+-- POSITIONS AND TRANSITIONS
+---------------------------------------------------------------------------------
+-- initial x and y position of left side
+leftSide.x = 0
+leftSide.y = 400
+
+-- make the left side collide with the right side.
+transition.to(leftSide, {x=350, y=400, time=1000})
+
+-- initial x and y position for the right side
+rightSide.x = 650
+rightSide.y = 400
